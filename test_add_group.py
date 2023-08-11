1597042f17ac26a19a52b5ff1beb7fdc50419355
Alexey Trushin <trushinav88@gmail.com>
2023-08-11 21:33:41 +0700

    update test_add_group.py

--- test_add_group.py
+++ test_add_group.py
@@ -8,13 +8,14 @@ from group import Group
 @pytest.fixture()
 def app(request):
     fixture = Application()
-    request.addfinilaizer(fixture.destroy)
+    request.addfinalizer(fixture.destroy)
     return fixture
 
 
 def test_add_group(app):
     app.open_home_page()
     app.login(username="admin", password="secret")
+    app.open_groups_page()
     app.create_group(Group(name="dfgdfgdg", header="dfgdfgdg", footer="dfgdfgdfg"))
     app.logout()
 
@@ -22,5 +23,6 @@ def test_add_group(app):
 def test_add_empty_group(app):
     app.open_home_page()
     app.login(username="admin", password="secret")
+    app.open_groups_page()
     app.create_group(Group(name="", header="", footer=""))
     app.logout()
