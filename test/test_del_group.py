db305f5feec9e9e3a0c60627fb29f36b289fc607
Alexey Trushin <trushinav88@gmail.com>
2023-08-20 21:35:10 +0700

    update for deleting groups

--- test/test_del_group.py
+++ test/test_del_group.py
@@ -1,5 +1,8 @@
 """test_delete_first_group"""
+from model.group import Group
 
 def test_delete_first_group(app):
     """test_delete_first_group"""
+    if app.group.count() == 0:
+        app.group.create(Group(name="New group"))
     app.group.delete_first_group()
