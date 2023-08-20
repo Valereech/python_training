12afb481b0ab82fb228425585d66189029f9c3c0
Alexey Trushin <trushinav88@gmail.com>
2023-08-21 01:02:44 +0700

    enforcement of preconditions for del group tests

--- test/test_mod_group.py
+++ test/test_mod_group.py
@@ -2,4 +2,6 @@ from model.group import Group
 
 
 def test_modify_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="New group"))
     app.group.modify_first_group(Group(name="Commerce", header="All sales", footer="All sales"))
