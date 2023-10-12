a58cc9d364a1bcc9f50749f0d572ab13ed813412
Aleksey Trushin <atrushin@ptsecurity.com>
2023-10-12 08:26:45 +0700

    update2 tests to add and delete user in/from group

--- test/test_del_user_from_group.py
+++ test/test_del_user_from_group.py
@@ -7,7 +7,13 @@ from fixture.orm import ORMfixture
 def test_delete_user_from_group(app):
     orm_db = ORMfixture(host='localhost', name='addressbook', user='root', password='')
     if len(orm_db.get_contact_list()) == 0:
-        app.user.create(User(firstname="New user"))
+        app.user.create(User(firstname="Alexey", middlename="Valeryevich", lastname="Trushin",
+                             nickname="Valereech", title="", company="PT",
+                             address="Tomsk", home="-", mobile="89231234567",
+                             work="-", fax="", email="alex224a@yandex.ru",
+                             email2="trushin88@gmail.com", email3="newmail_atrushin@mail.ru",
+                             homepage="", bday="13", bmonth="September",
+                             byear="1988", address2="-", phone2="-", notes=""))
     if len(orm_db.get_group_list()) == 0:
         app.group.create(Group(name="New group"))
     groups_list = orm_db.get_group_list()
